b07900fd5df7e3d6619856a7e5e53093ef58fb21
Ian Lewis <IanMLewis@gmail.com>
2011-08-19 14:06:05 +0900

    0.2 リリース

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
  
 setup (
     name='bpmailer',
-    version='0.1',
+    version='0.2',
     description='Mailing utility for Django',
     author='K.K. BeProud',
     author_email='ianmlewis@beproud.jp',
