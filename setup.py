ef2ecc433e3a1fb22420f791748af5f606d8cbd7
Ian Lewis <IanMLewis@gmail.com>
2012-05-28 18:21:35 +0900

    Django 1.4対応

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
  
 setup (
     name='bpmailer',
-    version='0.2',
+    version='0.3',
     description='Mailing utility for Django',
     author='K.K. BeProud',
     author_email='ianmlewis@beproud.jp',
