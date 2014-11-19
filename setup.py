364ae376455f0d483fecc0f9515de3f77b6538c8
Ian Lewis <IanMLewis@gmail.com>
2014-11-19 14:01:31 +0900

    Update version

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='bpmailer',
-    version='0.35',
+    version='0.36a',
     description='Mailing utility for Django',
     author='BeProud Inc.',
     author_email='ian@beproud.jp',
